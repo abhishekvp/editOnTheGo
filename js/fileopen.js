5f13679da136724beb4d431c59ebd149884b2ed3
Amod <amod.narvekar@rediffmail.com>
2013-01-08 11:19:14 -0800

    Create fileopen.js
    Contains the code of IMPORT from HTML

---
+++ js/fileopen.js
@@ -0,0 +1,11 @@
+  function readBlob() {
+    var files = document.getElementById('files').files;
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {  
+      if (evt.target.readyState == FileReader.DONE) {
+        document.getElementById('content').innerHTML = evt.target.result;
+      }	  
+    };
+    var blob = files[0].slice(0, files[0].size);
+	reader.readAsBinaryString(blob);
+  }
