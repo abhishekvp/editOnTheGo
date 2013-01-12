10fcf6c9d7a86f49885551db44844fbd98a5fdc7
Amod <amod.narvekar@rediffmail.com>
2013-01-12 04:15:27 -0800

    If user opens without choosing the file

--- js/fileopen.js
+++ js/fileopen.js
@@ -1,6 +1,8 @@
 
 function readBlob() {
     var files = document.getElementById('files').files;
+    if(!files.length)
+	return;    
     var reader = new FileReader();
     reader.onloadend = function(evt) {  
       if (evt.target.readyState == FileReader.DONE) {
