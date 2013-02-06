8581e045c6a556fda99730662968769e65079fa7
Abhishek Potnis <abhishekp2007@gmail.com>
2013-02-06 22:15:06 +0530

    fixed installation issue
    also removed test_menu folder and restructured mozapps.js

--- js/mozapp.js
+++ js/mozapp.js
@@ -1,27 +1,23 @@
-window.onload=function(){
-var request = navigator.mozApps.getSelf();
-var manLink = document.querySelector('link[rel="app-manifest"]'),
-manifestURL = manLink.getAttribute('href');
+function appInstall() {
+    var request = navigator.mozApps.getSelf();
+    var manLink = document.querySelector('link[rel="app-manifest"]'),
+        manifestURL = manLink.getAttribute('href');
 
-request.onsuccess = function() {
-  if (request.result) {
-    // we're installed
-  } else {
-    var request1 = navigator.mozApps.install(manifestURL);
-    request1.onsuccess = function() {
-  alert("App installed successfully !");
-}
-request1.onerror = function() {
- alert(this.error.name);
-  // whoops - this.error.name has details
-}
-  }
-}
-request.onerror = function() {
-  alert('Error checking installation status: ' + this.error.message);
-}
-   
+    request.onsuccess = function () {
+        if (request.result) {
+            console.log("Application already installed !");
+        } else {
+            var request = navigator.mozApps.install(manifestURL);
+            request.onsuccess = function () {
+                alert("Application installed successfully !");
+            }
+            request.onerror = function () {
+                alert(this.error.name);
+            }
+        }
+    }
+    request.onerror = function () {
+        alert('Error checking installation status: ' + this.error.message);
+    }
+
 };
-
-
-
