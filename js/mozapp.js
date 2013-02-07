6edaf4c53ab8dc264f7374d174a6412b64b005d8
Abhishek Potnis <abhishekp2007@gmail.com>
2013-02-07 18:38:31 +0530

    installation enabled

--- js/mozapp.js
+++ js/mozapp.js
@@ -2,11 +2,7 @@ function appInstall() {
     var request = navigator.mozApps.getSelf();
     var manLink = document.querySelector('link[rel="app-manifest"]'),
         manifestURL = manLink.getAttribute('href');
-
     request.onsuccess = function () {
-        if (request.result) {
-            console.log("Application already installed !");
-        } else {
             var request = navigator.mozApps.install(manifestURL);
             request.onsuccess = function () {
                 alert("Application installed successfully !");
@@ -15,7 +11,6 @@ function appInstall() {
                 alert(this.error.name);
             }
         }
-    }
     request.onerror = function () {
         alert('Error checking installation status: ' + this.error.message);
     }
