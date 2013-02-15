9fe7c242b3a1c00d47e9f1ec239e4206bf0c18f5
Abhishek Potnis <abhishekp2007@gmail.com>
2013-02-15 18:56:46 +0530

    trying to fix install issue

--- js/mozapp.js
+++ js/mozapp.js
@@ -1,18 +1,22 @@
 function appInstall() {
-    var request = navigator.mozApps.getSelf();
-    var manLink = document.querySelector('link[rel="app-manifest"]'),
-        manifestURL = manLink.getAttribute('href');
-    request.onsuccess = function () {
-            var request = navigator.mozApps.install(manifestURL);
-            request.onsuccess = function () {
+    var requestIns = navigator.mozApps.getSelf();
+    requestIns.onsuccess = function () {
+        console.log("requestIns = " + requestIns.result);
+        if (requestIns.result) {
+            alert("Already Installed");
+        } else {
+            var manLink = document.querySelector('link[rel="app-manifest"]'),
+                manifestURL = manLink.getAttribute('href');
+            var requestChk = navigator.mozApps.install(manifestURL);
+            requestChk.onsuccess = function () {
                 alert("Application installed successfully !");
             }
-            request.onerror = function () {
+            requestChk.onerror = function () {
                 alert(this.error.name);
             }
         }
-    request.onerror = function () {
+    }
+    requestIns.onerror = function () {
         alert('Error checking installation status: ' + this.error.message);
     }
-
 };
