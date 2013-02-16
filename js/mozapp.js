aa495be2315267a2600de1a81504aadc2aae0fae
Abhishek Potnis <abhishekp2007@gmail.com>
2013-02-16 09:05:07 +0530

    removed alert " Already Installed"

--- js/mozapp.js
+++ js/mozapp.js
@@ -1,9 +1,8 @@
 function appInstall() {
     var requestIns = navigator.mozApps.getSelf();
     requestIns.onsuccess = function () {
-        console.log("requestIns = " + requestIns.result);
         if (requestIns.result) {
-            alert("Already Installed");
+            console.log("Already Installed");
         } else {
             var manLink = document.querySelector('link[rel="app-manifest"]'),
                 manifestURL = manLink.getAttribute('href');
