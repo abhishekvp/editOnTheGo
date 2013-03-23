9a5d890a03ab80407c8dcc33d7c11343862c5495
Amod <amod.narvekar@rediffmail.com>
2013-03-23 09:31:54 -0700

    exporting a file.

---
+++ drawer/js/export.js
@@ -0,0 +1,34 @@
+var htmlFileName;
+function exportFile()
+{
+ htmlFileName=fn;
+ //alert("filename="+htmlFileName);
+ navigator.mozSetMessageHandler('activity', function(activity) {
+   var name=activity.source.name;
+   if(name=="pick")
+   {
+   var blob=readBlob();
+   if (!text) {
+     a.postError("NoTextFile");
+     return;
+   }
+   alert('successfull');
+   activity.postResult({ type: "text/html", blob: blob });
+   }
+ });
+}
+
+function readBlob()
+{
+  var blob;
+	var file = htmlFileName;
+	var reader = new FileReader();
+	
+	reader.onloadend = function(evt) {
+    if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+		blob=evt.target.result;
+	}
+    };
+	//reader.readAsBinaryString(blob);
+	return blob;
+}  
