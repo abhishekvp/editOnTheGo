b17999cc713f2479937c54ff59a297b21af7e8ec
Amod <amod.narvekar@rediffmail.com>
2013-03-27 21:16:14 -0700

    Converted the text to BLOB !!!

--- drawer/js/export.js
+++ drawer/js/export.js
@@ -1,17 +1,14 @@
-var htmlFileName;
-window.onload = function() {
- htmlFileName=fn;
- navigator.mozSetMessageHandler('activity', function(activity) {
+
+function exportFile() {
+   navigator.mozSetMessageHandler('activity', function(activity) {
+
    var name=activity.source.name;
    if(name != "pick")
    return;
    else
    {
    var blob=readBlob();
-   if (!text) {
-     a.postError("NoTextFile");
-     return;
-   }
+   alert("File exported successfully.");
    activity.postResult({ type: "text/html", blob: blob });
    }
  });
@@ -20,14 +17,6 @@ window.onload = function() {
 function readBlob()
 {
 	var blob;
-	var file = htmlFileName;
-	var reader = new FileReader();
-	
-	reader.onloadend = function(evt) {
-    if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-		blob=evt.target.result;
-	}
-    };
-	reader.readAsBinaryString(blob);
-	return blob;
+	var file = fn;
+	return new Blob([text1], { type: 'text/html' });
 }  
