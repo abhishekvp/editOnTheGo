ff7fbb9224208fe6a6982a888cbf808180273318
Amod <amod.narvekar@rediffmail.com>
2013-03-27 22:27:45 -0700

    Update export.js

--- drawer/js/export.js
+++ drawer/js/export.js
@@ -1,5 +1,4 @@
 function exportFile() {
-	alert("exportFile entered");
    navigator.mozSetMessageHandler('activity', function(activity) {
 
    var name=activity.source.name;
@@ -16,7 +15,6 @@ function exportFile() {
 
 function readBlob()
 {
-	alert("readBlob entered");
 	var blob;
 	var file = fn;
 	return new Blob([text1], { type: 'text/html' });
